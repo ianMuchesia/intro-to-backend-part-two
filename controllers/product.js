b617497ae6b9bf6a9b114b11f2175a4c69d8b649
ianMuchesia <ianmuchesia@yahoo.com>
2023-01-16 13:30:31 +0300

    final

--- controllers/product.js
+++ controllers/product.js
@@ -1,12 +1,32 @@
+const products = require('../models/products')
+const Product = require('../models/products')
 
+
+
+const getAllProductsStatic = async (req, res)=>{
+    const products = await Product.find({name:"a first wooden table" })
+    //throw new Error('testing async error')
+    res.status(200).json({products, nbHits:products.length})
+}
 
 const getAllProducts = async (req, res)=>{
-    res.status(200).json({msg:"product route"})
+
+    const {featured, company } = req.query
+
+    const queryObject = {}
+
+    if(featured){
+        queryObject.featured = featured === true? true: false;
+    }
+    if(company){
+        queryObject.company = company
+    }
+
+    console.log(queryObject)
+    const products = await Product.find(queryObject)
+   res.status(200).json({products, nbHits:products.length})
 }
-const getAllProductsStatic = async (req, res)=>{
-    throw new Error('testing async error')
-    res.status(200).json({msg:"product route"})
-}
+
 
 module.exports = {
     getAllProducts,getAllProductsStatic,
